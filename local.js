ce3de390ae89706f7e1292b97c6796bb95a083ea
yashwanthkkn <yashwanthkkn>
2021-04-17 23:06:23 +0530

    changed local to app service

--- local.js
+++ local.js
@@ -5,12 +5,12 @@ const handler = require('./lib/app')
 app.use(handler)
 
 app.listen(process.env.PORT || 3000,()=>{
-    console.log("Server(local) Running at http://localhost:3000/")  
-    require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-        if(err){
-            console.log(err);
-        }else{
-            console.log(`Server(network) Running at http://${add}:3000/`)
-        }
-      })
+    console.log(`Server(local) Running at http://localhost:${process.env.PORT}/`)  
+    // require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    //     if(err){
+    //         console.log(err);
+    //     }else{
+    //         console.log(`Server(network) Running at http://${add}:3000/`)
+    //     }
+    //   })
 })
